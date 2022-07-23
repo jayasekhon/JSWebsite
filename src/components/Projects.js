168a923b6746e8a9d9291a1ba663a87a696b60d2
jayasekhon <60359284+jayasekhon@users.noreply.github.com>
2022-07-23 21:40:33 +0100

    added in written text and game location

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,12 +20,27 @@ class Projects extends Component {
       var sectionName = this.props.resumeBasicInfo.section_name.projects;
       var projects = this.props.resumeProjects.map(function (projects) {
         return (
+          <div >
+          <h2 className="project-text col-3">
+                BirdGang was created as part of the Unviersity of Bristol Computer Science MEng Games Project. It was awarded a first class mark, and judged as “outstanding” by the panel, entering “truly professional territory".
+
+                <br/><br/>
+                I was selected to be Team Manager of the project from its inception. Given the scope of the project, a successful outcome required detailed planning and a common vision within the team. At the start, I ensured we spent time discussing our initial ideas, and implemented the Scrum model of Agile development to ensure user feedback analysis and team progress was communicated regularly during weekly Sprint Review and Retrospective sessions. This ensured we maintained consistent progress and met each deadline to a high standard. It also made certain all team members felt heard, respected, and free to voice their opinions without judgement. When inevitable disagreements arose, I led discussions and formed actionable points, establishing a culture of constructive criticism and candid feedback within the group. I also ensured we took a User-Centred Design approach, keeping the needs of end users at the forefront throughout. I discuss our project development strategies in more detail in the video seen to the left @ 5:20
+
+
+                <br/><br/>
+                In the game, players enter the world as BirdGang recruits and are tasked with finding bad AI people and ruining their day.  Players will face four miniboss rounds in the games duration. In each, they must work together to defeat them, using waypoints and help messages to communicate with one another. Each of the two-minute rounds are introduced by a cinematic cutscene and an audio overlay from the gang boss. AI bird flocks also populate the sky and target any players who remain still for too long. The game makes use of advanced AI systems, intuitive flight control scheme grounded in a physics engine, and includes over 52 unique models built in-house.
+                <br/><br/>
+                Click below to play the game (recommended browser is Chrome, game will not run on Safari):
+                <br/><br/>
+                https://jayasekhon.github.io/BirdGang/
+                <br/><br/></h2>
           <div
             className="col-sm-12 col-md-6 col-lg-4"
             key={projects.title}
             style={{ cursor: "pointer" }}
           >
-            <div className="project-text"><h2 >test of positioning</h2></div>
+  
             <span className="portfolio-item d-block">
               <div className="foto" onClick={() => detailsModalShow(projects)}>
                 <div>
@@ -42,7 +57,7 @@ class Projects extends Component {
                   </p>
                   <iframe
                 width="682"
-                height="384"
+                height="400"
                 src="https://www.youtube.com/embed/aOmKyhrso0Q"
                 frameborder="0"
                 allow="autoplay; encrypted-media"
@@ -53,6 +68,7 @@ class Projects extends Component {
               </div>
             </span>
            
+          </div>
           </div>
           
         );
